5f47d14779b5993b2eba0660486b35578665cfe1
Koohyewon <khw6746@naver.com>
2024-07-19 02:39:21 +0900

    Change default screen size

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <></>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
