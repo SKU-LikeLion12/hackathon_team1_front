fef9055bcae7399837972c60cc004136fac3049f
Koohyewon <khw6746@naver.com>
2024-07-24 02:37:28 +0900

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <div className="h-16 border-b-2 flex justify-center items-center">
+        무연 Logo
+      </div>
+    </>
+  );
+}
