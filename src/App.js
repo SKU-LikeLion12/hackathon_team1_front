d99676ea34f9c29552abdec2240d93acf0f8820e
kimchaeyeonn <betty6994@naver.com>
2024-08-01 16:15:06 +0900

    design community

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
+import { Link } from 'react-router-dom';
+import Community from "./pages/Community";
+import Post from "./pages/Post"
+import PostDetail from "./pages/PostDetail";
+import PostPhoto from "./pages/PostPhoto";
+
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/post/:postId" element={<PostDetail />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/postdetail" element={<PostDetail />} />
+          <Route path="/postphoto" element={<PostPhoto />} />
         </Routes>
       </BrowserRouter>
     </div>
