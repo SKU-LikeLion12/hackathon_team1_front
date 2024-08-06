463020b65249d1db2c60cf8c04eb93dc747024a1
Koohyewon <khw6746@naver.com>
2024-08-06 23:12:40 +0900

    https

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -4,7 +4,7 @@ export default function api() {
   const token = localStorage.getItem("token");
 
   //baseURL 설정 전
-  const instance = axios.create({ baseURL: "http://team1back.sku-sku.com" });
+  const instance = axios.create({ baseURL: "https://team1back.sku-sku.com" });
 
   if (token) {
     instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
